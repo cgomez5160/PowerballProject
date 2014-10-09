4a4af0ac46d8d3b4e28517c6380f9831ff93c80d
riku12124 <riku12124@hotmail.com>
2014-10-09 11:01:38 -0400

    Initial Commit

---
+++ powerballProject/powerballProject/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace powerballProject
+{
+	 class Program
+	 {
+		  /*
+		   * Objective:
+		   *		 Create a powerball number generator.
+		   *		 <add more info>
+		   * 
+		   * 
+		   */
+		  static void Main(string[] args)
+		  {
+				//Enter Code
+		  }
+	 }
+}
